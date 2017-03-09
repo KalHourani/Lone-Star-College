4884acf1792f696f16d764c828305bb778c7f958
Khalid Hourani <khalid.hourani@gmail.com>
2017-03-09 17:47:25 -0600

    new file:   Discussions/Analysis of Algorithms/Graph.png 	new file:   Discussions/Analysis of Algorithms/bin_exp.py 	new file:   Discussions/Analysis of Algorithms/data_points.dat 	new file:   Discussions/Analysis of Algorithms/main.pdf 	modified:   Discussions/Analysis of Algorithms/main.tex 	new file:   Projects/Project 1/binaryPow.cpp 	new file:   Projects/Project 1/isPrime.cpp 	modified:   Projects/Project 1/main.pdf 	modified:   Projects/Project 1/main.tex 	new file:   Projects/Project 1/recursivePow.cpp

---
+++ Projects/Project 1/recursivePow.cpp
@@ -0,0 +1,11 @@
+double pow(double x, int n)
+{
+        if (n == 0)
+        {
+                return 1;
+        }
+        else
+        {
+                return x * pow(x, n - 1);
+        }
+}
