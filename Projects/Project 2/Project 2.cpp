5136074f2799ae3d22f950196694d4ae3c1a62fa
Khalid M. Hourani <khourani@hsawh.org>
2017-04-04 14:47:30 -0500

    modified:   Project 2.cpp

--- Projects/Project 2/Project 2.cpp
+++ Projects/Project 2/Project 2.cpp
@@ -12,27 +12,27 @@ using namespace std;
 //function prototypes
 void enter_prompt();
 void clear_input();
+void demonstration();
+string tolower(string);
 
 int main()
 {
-	spellChecker S = spellChecker();
-	string word;
-	cout << "Input a word.";
-	getline(cin, word);
-	vector<string> k = S.spellCheck(word);
-	if (k.size() == 1)
+	int input = 1;
+	while (input == 1)
 	{
-		cout << "Your word is spelled correctly." << endl;
-		enter_prompt();
-	}
-	else
-	{
-		cout << "Your word appears to be misspelled. Did you mean:" << endl;
-		vector<string>::iterator it;
-		for (it = k.begin(); it != k.end(); ++it)
+		cout << "Choose an option:" << endl;
+		cout << "1. Check the spelling of a word." << endl;
+		cout << "2. Terminate the program." << endl;
+		cin >> input;
+		if (input == 1)
 		{
-			cout << *it << endl;
+			clear_input();
+			demonstration();
+			clear_input();
 		}
+	}
+	if (input == 2)
+	{
 		enter_prompt();
 	}
     return 0;
@@ -50,4 +50,42 @@ void clear_input() //reset cin flags and clear standard input
 {
 	cin.clear(); //reset cin flags
 	cin.ignore(numeric_limits<streamsize>::max(), '\n'); //clear cin
+}
+
+string tolower(string s)
+{
+	string new_s = s;
+	for (int i = 0; i < s.size(); i++)
+	{
+		new_s[i] = tolower(new_s[i]);
+	}
+	return new_s;
+}
+
+void demonstration()
+{
+	spellChecker S = spellChecker();
+	string word;
+	cout << "Input a word." << endl;
+	getline(cin, word);
+	word = tolower(word);
+	vector<string> k = S.spellCheck(word);
+	int length = k.size();
+	if (length == 0)
+	{
+		cout << "Your word appears to be misspelled. No suggested spelling is available.";
+	}
+	else if (k.size() == 1)
+	{
+		cout << "Your word is spelled correctly." << endl;
+	}
+	else
+	{
+		cout << "Your word appears to be misspelled. Did you mean:" << endl;
+		vector<string>::iterator it;
+		for (it = k.begin(); it != k.end(); ++it)
+		{
+			cout << *it << endl;
+		}
+	}
 }
